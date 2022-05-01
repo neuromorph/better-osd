e957091ca6aaa2a6415226d96274768fb55a7c36
neuromorph <pradeep.gaidhani@gmail.com>
2022-05-01 19:02:20 +0530

    Fixed extension disable for Gnome 42

--- extension.js
+++ extension.js
@@ -57,6 +57,7 @@ function removeInjection(object, injection, name) {
 
 let injections = [];
 let _id;
+let _size = [];
 
 //---------------------------------------------
 
@@ -77,16 +78,19 @@ function enable() {
       let v_percent = _settings.get_int("vertical");
       let osd_size = _settings.get_int("size");
       let hide_delay = _settings.get_int("delay");
+      imports.ui.osdWindow.HIDE_TIMEOUT = hide_delay;
+      
       let transparency = _settings.get_boolean("transparency");
       transparency ? style() : unstyle();
 
       this._hbox.translation_x = (h_percent * monitor.width) / 100;
       this._hbox.translation_y = (v_percent * monitor.height) / 100;
 
+      _size.push(this._icon.icon_size);
       this._icon.icon_size = (osd_size * monitor.height) / 100 / 2;
       this._hboxConstraint._minSize = (osd_size * monitor.height) / 100;
 
-      imports.ui.osdWindow.HIDE_TIMEOUT = hide_delay;
+      
     }
   );
 }
@@ -97,10 +101,13 @@ function disable() {
 
   let arrayOSD = Main.osdWindowManager._osdWindows;
   for (let i = 0; i < arrayOSD.length; i++) {
-    arrayOSD[i]._relayout();
+    
     arrayOSD[i]._hbox.translation_x = 0;
-    imports.ui.osdWindow.HIDE_TIMEOUT = 1500;
+    arrayOSD[i]._hbox.translation_y = 0;
+    arrayOSD[i]._icon.icon_size = _size[i];
+    
   }
+  imports.ui.osdWindow.HIDE_TIMEOUT = 1500;
 
   removeInjection(OsdWindow.OsdWindow.prototype, injections, "show");
 }
